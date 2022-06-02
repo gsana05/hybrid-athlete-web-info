d95c2b5556aa5c8348adf619e8432c223e073891
sanas <gareth.sanashee@apptaura.com>
2022-06-02 07:36:33 -0700

    login ui

--- src/features/adminLogIn/AdminLogin.tsx
+++ src/features/adminLogIn/AdminLogin.tsx
@@ -1,10 +1,49 @@
 import './adminLogin.scss';
+import React, { useRef, useState, useEffect  } from "react";
 
 export function AdminLogIn()  {
 
+    const logAdminUserIn = () => {
+        console.log("Email: " + email);
+        console.log("Password: " + password);
+
+    }
+
+    // set & get email
+    const [email, setEmail] = useState("");
+
+    function setEmailInput(event : any) {
+        const input = (event.target as any).value;
+        setEmail(input);
+    }
+
+    // set & get password
+    const [password, setPassword] = useState("");
+
+    function setPasswordInput(event : any) {
+        const input = (event.target as any).value;
+        setPassword(input);
+    }
+
     return(
-        <div>
-            <h1>Admin login </h1>
+        <div className='admin-login-container'>
+            
+
+            <div className='login-container'>
+
+                <h1>Admin Login</h1>
+
+                <p>Email</p>
+                <input className='measurements input' type="email"  onChange={setEmailInput} value={email} />
+
+                <p>Password</p>
+                <input className='measurements input'  type="password" onChange={setPasswordInput} value={password} />
+
+                <button className='login-button measurements' onClick={logAdminUserIn}>Login</button>
+
+            </div>
+
+
         </div>
     )
 }
